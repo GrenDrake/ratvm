a3d538a8be87504933cdd3dc2a298e2efd65fa64
Gren Drake <grendrake@gmail.com>
2019-01-03 23:41:17 -0800

    Fixes alignment of opcode values

--- src/parse_functions.cpp
+++ src/parse_functions.cpp
@@ -26,32 +26,32 @@
 
 struct OpcodeDef {
     enum CodeOpcode {
-        Return      = 0,
-        Push0       = 1,
-        Push1       = 2,
-        PushNeg1    = 3,
-        Push8       = 4,
-        Push16      = 5,
-        Push32      = 6,
-        Store       = 7,
-        Say         = 10,
-        SayUnsigned = 11,
-        SayChar     = 12,
-        StackPop    = 13, // remove the top item from the stack
-        StackDup    = 14, // duplicate the top item on the stack
-        StackPeek   = 15, // peek at the stack item X items from the top
-        StackSize   = 16, // get the current size of the stack
-        Call        = 17, // call a value as a function
-        CallMethod  = 18, // call an object property as a function
-        Self        = 19, // get object the current function is a property of
-        GetProp     = 20,
-        HasProp     = 21, // check if property is set on object
-        SetProp     = 22, // set object property to value
-        GetItem     = 23, // get item from list (index) or map (key)
-        HasItem     = 24, // check if index (for list) or key (for map) exists
-        GetSize     = 25, // get size of list or map
-        SetItem     = 26, // set item in list (by index) of map (by key)
-        TypeOf      = 27, // get value type
+        Return              = 0,
+        Push0               = 1,
+        Push1               = 2,
+        PushNeg1            = 3,
+        Push8               = 4,
+        Push16              = 5,
+        Push32              = 6,
+        Store               = 7,
+        Say                 = 10,
+        SayUnsigned         = 11,
+        SayChar             = 12,
+        StackPop            = 13, // remove the top item from the stack
+        StackDup            = 14, // duplicate the top item on the stack
+        StackPeek           = 15, // peek at the stack item X items from the top
+        StackSize           = 16, // get the current size of the stack
+        Call                = 17, // call a value as a function
+        CallMethod          = 18, // call an object property as a function
+        Self                = 19, // get object the current function is a property of
+        GetProp             = 20,
+        HasProp             = 21, // check if property is set on object
+        SetProp             = 22, // set object property to value
+        GetItem             = 23, // get item from list (index) or map (key)
+        HasItem             = 24, // check if index (for list) or key (for map) exists
+        GetSize             = 25, // get size of list or map
+        SetItem             = 26, // set item in list (by index) of map (by key)
+        TypeOf              = 27, // get value type
         CompareTypes        = 30, // compare the types of two values and push the result
         Compare             = 31, // compare two values and push the result
         Jump                = 32, // unconditional jump
@@ -61,10 +61,10 @@ struct OpcodeDef {
         JumpLessThanEqual   = 36, // jump if top of stack <= 0
         JumpGreaterThan     = 37, // jump if top of stack > 0
         JumpGreaterThanEqual= 38, // jump if top of stack >= 0
-        Add         = 40,
-        Sub         = 41,
-        Mult        = 42,
-        Div         = 43,
+        Add                 = 40,
+        Sub                 = 41,
+        Mult                = 42,
+        Div                 = 43,
         WaitKey             = 50,
     };
 
