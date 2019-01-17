eeac4e1131b9a507606992246bb6d023dd564bb6
Gren Drake <grendrake@gmail.com>
2019-01-17 01:19:09 -0800

    Fixes value of predefined symbol "Label"

--- src/translate.cpp
+++ src/translate.cpp
@@ -22,7 +22,7 @@ void add_default_constants(GameData &gamedata) {
     gamedata.symbols.add(SymbolDef(Origin(), "Function",        Value{Value::Integer, 5}));
     gamedata.symbols.add(SymbolDef(Origin(), "Object",          Value{Value::Integer, 6}));
     gamedata.symbols.add(SymbolDef(Origin(), "Property",        Value{Value::Integer, 7}));
-    gamedata.symbols.add(SymbolDef(Origin(), "Label",           Value{Value::Integer, 7}));
+    gamedata.symbols.add(SymbolDef(Origin(), "Label",           Value{Value::Integer, 9}));
     gamedata.symbols.add(SymbolDef(Origin(), "InfobarLeft",     Value{Value::Integer, 0}));
     gamedata.symbols.add(SymbolDef(Origin(), "InfobarRight",    Value{Value::Integer, 1}));
     gamedata.symbols.add(SymbolDef(Origin(), "InfobarFooter",   Value{Value::Integer, 2}));
