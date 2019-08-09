04f86c48cdbaaf0e0d205366388030d9e1f3e516
Gren Drake <gren@grenslair.com>
2019-08-09 07:06:40 -0700

    Adds str_append_uf opcode

--- build/opcode.cpp
+++ build/opcode.cpp
@@ -63,6 +63,7 @@ OpcodeDef opcodes[] = {
     {   "add_option",   OpcodeDef::AddOption,               4, 0 },
     {   "str_clear",    OpcodeDef::StringClear,             1, 0 },
     {   "str_append",   OpcodeDef::StringAppend,            2, 0 },
+    {   "str_append_uf",OpcodeDef::StringAppendUF,          2, 0 },
     {   "str_length",   OpcodeDef::StringLength,            1, 1 },
     {   "str_compare",  OpcodeDef::StringCompare,           2, 1 },
     {   "error",        OpcodeDef::Error,                   1, 0 },
