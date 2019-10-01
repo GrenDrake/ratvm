81c95ef8aa154d6be885af149130d4f6e280c791
Gren Drake <grendrake@gmail.com>
2019-10-01 02:21:27 -0700

    Adds collect and is_valid opcodes

--- build/opcode.h
+++ build/opcode.h
@@ -17,7 +17,7 @@ struct OpcodeDef {
         Push16              = 5,
         Push32              = 6,
         Store               = 7,
-        /* unused: 8 */
+        CollectGarbage      = 8,
         SayUCFirst          = 9,
         Say                 = 10,
         SayUnsigned         = 11,
@@ -27,7 +27,7 @@ struct OpcodeDef {
         StackPeek           = 15, // peek at the stack item X items from the top
         StackSize           = 16, // get the current size of the stack
         Call                = 17, // call a value as a function
-        /* unused: 18 */
+        IsValid             = 18,
         ListPush            = 19,
         ListPop             = 20,
         Sort                = 21,
